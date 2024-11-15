31ef40bc2bb7d92d24b464c257438f23e2a52610
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2024-11-15 10:31:00 +0100

    CSS positioning of the stamp and signature.

--- resources/views/parts/pdf-invoice-style.blade.php
+++ resources/views/parts/pdf-invoice-style.blade.php
@@ -113,11 +113,13 @@
     }
     .mp-image {
         position: absolute;
+        top: 0px;
         left: 50%;
         transform: translateX(-50%); /* Center within the cell */
     }
     .signature-image {
         position: absolute;
+        top:0px;
         left: 50%;
         transform: translateX(-50%); /* Center within the cell */
     }
