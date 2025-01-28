359691aff8e651baaa5c1b4a6f3f2c33749c23cb
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-01-28 12:17:40 +0100

    Visuals.

--- resources/views/orders-edit.blade.php
+++ resources/views/orders-edit.blade.php
@@ -32,7 +32,7 @@
                 @endif
 
             @else
-              <a class="btn btn-primary btn-sm" style="font-weight:bold; font-size:100%" href="#"><i class="bi bi-plus-square-fill"></i> Napravi</a>
+              <a class="btn btn-primary btn-sm" style="font-weight:bold; font-size:100%" href="#"><i class="bi bi-plus-square"></i> Napravi</a>
             @endif
           </span>
           {{-- Header right side --}}
