0cbe409edff0598a83695a11891d66c49e9216e6
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-01-29 12:23:19 +0100

    Added modal for quick invoice on orders-edit.

--- resources/views/orders-edit.blade.php
+++ resources/views/orders-edit.blade.php
@@ -460,6 +460,47 @@
   </div>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="invoiceModal" tabindex="-1" aria-labelledby="invoiceModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="invoiceModalLabel">Novi račun</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <!-- popup content -->
+        <form method="POST" action="/racuni" id="receiptSubmission">
+          {{ csrf_field() }}
+              <div class="form-group">
+
+                <div class="mb-3">
+                  <label for="number">Redni broj računa:</label>
+                  <input type="number" class="form-control" placeholder="Unesi redni broj računa..." id="number" name="number" value="{{ $latest }}">
+                </div>
+
+                <input type="hidden" placeholder="Unesi redni broj računa..." id="order_id" name="order_id" value="{{ $orderId }}">
+
+                <div class="mb-3">
+                  <label for="year">Godina računa:</label>
+                  <select class="form-select searchable-select-modal" id="year" name="year">
+                    @foreach ($workYears as $workYear)
+                      <option {{ $loop->last ? 'selected' : '' }}>{{ $workYear->year }}</option>
+                    @endforeach
+                  </select>
+                </div>
+
+              </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zatvori</button>
+        <button type="submit" class="btn btn-primary" form="receiptSubmission">Spremi</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 @include('parts.deleteconfirmation')
 
 @endsection
