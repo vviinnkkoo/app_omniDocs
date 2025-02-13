3d82d341435ff978fd024d78edc3aac2d89d7368
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-02-13 09:38:32 +0100

    Code optimization.

--- app/Http/Controllers/DoomPDFController.php
+++ app/Http/Controllers/DoomPDFController.php
@@ -7,165 +7,120 @@ use App\Models\Order;
 use App\Models\OrderItemList;
 use App\Models\DeliveryService;
 use App\Models\PrintLabel;
-
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 class DoomPDFController extends Controller
 {
-    // Protect all functions and redirect to login if necessary
-    public function __construct(OrderItemListController $orderItemListController, Omnicontrol $omnicontrol)
+    public function __construct(OrderItemListController $orderItemListController)
     {
         $this->middleware('auth');
         $this->orderItemListController = $orderItemListController;
-        $this->omnicontrol = $omnicontrol;
     }
-
-
 
     protected $orderItemListController;
     protected $omnicontrol;
-    
 
+    private function calculateTotal($order_id)
+    {
+        $subtotal = $this->orderItemListController->sumOrderItemList($order_id);
+        $deliveryService = DeliveryService::where('id', Order::find($order_id)->delivery_service_id)->firstOrFail();
+        $deliveryCost = str_replace(',', '.', $deliveryService->default_cost);
+        return [
+            'subtotal' => $subtotal,
+            'deliveryCost' => $deliveryCost,
+            'total' => number_format(($subtotal + $deliveryCost), 2, ',', '.'),
+            'deliveryCostFormated' => number_format($deliveryCost, 2, ',', '.')
+        ];
+    }
 
-    public function invoice($id) {
-
+    public function invoice($id)
+    {
         $receipt = Receipt::where('id', $id)->firstOrFail();
         $order = Order::where('id', $receipt->order_id)->firstOrFail();
         $orderItemList = OrderItemList::where('order_id', $receipt->order_id)->get();
-        $deliveryService = DeliveryService::where('id', $order->delivery_service_id)->firstOrFail();
-
-        $hp_cod_modifier = $this->omnicontrol->hpCodModifierCheck($order->id); // only for company with id "1"
-        $subtotal = $this->orderItemListController->sumOrderItemList($order->id);
-
-        $deliveryCost = str_replace(',', '.', $deliveryService->default_cost);
-        $deliveryCost = $deliveryCost - $hp_cod_modifier;
-        $deliveryCostFormated = number_format($deliveryCost, 2, ',', '.');
-
-        $total = number_format(($subtotal + $deliveryCost), 2, ',', '.');
-        
-        $date = date("dmY-Gis"); // Date for name
+        $totals = $this->calculateTotal($order->id);
+        $date = date("dmY-Gis");
 
         $pdf = Pdf::loadView('pdf.invoice', [
             'receipt' => $receipt,
             'order' => $order,
             'orderItemList' => $orderItemList,
-            'deliveryService' => $deliveryService,
-            'total' => $total,
-            'subtotal' => $subtotal,
-            'deliveryCost' => $deliveryCostFormated
+            'deliveryService' => $order->deliveryService,
+            'total' => $totals['total'],
+            'subtotal' => $totals['subtotal'],
+            'deliveryCost' => $totals['deliveryCostFormated']
         ]);
 
         return $pdf->stream('račun-' . $receipt->year . '-' . $receipt->number . '-1-1-' . $date . '.pdf');
-
     }
 
-
-
-    public function documents($mode, $id) {
-
+    public function documents($mode, $id)
+    {
         $order = Order::where('id', $id)->firstOrFail();
         $orderItemList = OrderItemList::where('order_id', $id)->get();
-        $deliveryService = DeliveryService::where('id', $order->delivery_service_id)->firstOrFail();
+        $totals = $this->calculateTotal($order->id);
+        $date = date("dmY-Gis");
 
-        $hp_cod_modifier = $this->omnicontrol->hpCodModifierCheck($order->id); // only for company with id "1"
-        $subtotal = $this->orderItemListController->sumOrderItemList($order->id);
-
-        $deliveryCost = str_replace(',', '.', $deliveryService->default_cost);
-        $deliveryCost = $deliveryCost - $hp_cod_modifier;
-        $deliveryCostFormated = number_format($deliveryCost, 2, ',', '.');
-
-        $total = number_format(($subtotal + $deliveryCost), 2, ',', '.');
-
-
-        $date = date("dmY-Gis"); // Date for name 
+        $view = '';
+        $filename = '';
 
         switch ($mode) {
-
             case 'otpremnica':
-
-                $pdf = Pdf::loadView('pdf.dispatch', [
-                    'order' => $order,
-                    'orderItemList' => $orderItemList,
-                    'deliveryService' => $deliveryService,
-                    'total' => $total,
-                    'subtotal' => $subtotal,
-                    'deliveryCost' => $deliveryCostFormated
-                ]);
-    
-                return $pdf->stream('otpremnica-' . $id . '-' . $date . '.pdf');
-            
+                $view = 'pdf.dispatch';
+                $filename = 'otpremnica-' . $id . '-' . $date . '.pdf';
+                break;
             case 'ponuda':
-
-                    $pdf = Pdf::loadView('pdf.quotation', [
-                        'order' => $order,
-                        'orderItemList' => $orderItemList,
-                        'deliveryService' => $deliveryService,
-                        'total' => $total,
-                        'subtotal' => $subtotal,
-                        'deliveryCost' => $deliveryCostFormated
-                    ]);
-
-                    return $pdf->stream('ponuda-' . $id . '-' . $date . '.pdf');
-                
+                $view = 'pdf.quotation';
+                $filename = 'ponuda-' . $id . '-' . $date . '.pdf';
+                break;
             default:
-
                 return redirect('/');
-                
         }
 
-            
-        
+        $pdf = Pdf::loadView($view, [
+            'order' => $order,
+            'orderItemList' => $orderItemList,
+            'deliveryService' => $order->deliveryService,
+            'total' => $totals['total'],
+            'subtotal' => $totals['subtotal'],
+            'deliveryCost' => $totals['deliveryCostFormated']
+        ]);
 
+        return $pdf->stream($filename);
     }
 
-
-
-    public function shippingLabels() {
+    public function shippingLabels()
+    {
         $shippingLabels = PrintLabel::where('label_type', 'shipping')->get();
-        
+
         $pdf = Pdf::loadView('pdf.shippingLabels', [
             'shippingLabels' => $shippingLabels
         ]);
-     
+
         return $pdf->stream();
     }
 
+    public function p10mLabels($id)
+    {
+        $order = Order::where('id', $id)->firstOrFail();
+        $customPaper = [0, 0, 396.85, 563.15];
 
-
-    public function p10mLabels($id) {
-
-        $order = Order::where('id', $id)->firstOrFail();;
-        $customPaper = array(0,0,396.85,563.15);
-        
-        $pdf = new Pdf();
-        
         $pdf = Pdf::loadView('pdf.p10mLabels', [
-             'order' => $order
-         ]);
+            'order' => $order
+        ]);
 
-         $pdf->setPaper($customPaper);
+        $pdf->setPaper($customPaper);
 
-        
-     
         return $pdf->stream();
     }
 
-
-
-    public static function labelItemTotal($order_id, $omnicontrol, $orderItemListController) {
-
-        $receipt = Receipt::where('order_id', $order_id)->firstOrFail();
-        $order = Order::where('id', $order_id)->firstOrFail();
-        $deliveryService = DeliveryService::where('id', Order::find($order_id)->delivery_service_id)->firstOrFail();
-
-        $hp_cod_modifier = $omnicontrol->hpCodModifierCheck($order_id); // only for company with id "1"
+    public static function labelItemTotal($order_id, $omnicontrol, $orderItemListController)
+    {
         $subtotal = $orderItemListController->sumOrderItemList($order_id);
-
+        $deliveryService = DeliveryService::where('id', Order::find($order_id)->delivery_service_id)->firstOrFail();
         $deliveryCost = str_replace(',', '.', $deliveryService->default_cost);
-        $deliveryCost = $deliveryCost - $hp_cod_modifier;
-        
+
         return number_format(($subtotal + $deliveryCost), 2, ',', '.');
     }
-
 }
