7d32a8af6efce2e5834e46265ef1d5baf332e70c
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-02-14 09:44:27 +0100

    Code optimization, fillable fields.

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -9,8 +9,14 @@ class Customer extends Model
 {
     use HasFactory;
 
+    // Specify which attributes are mass-assignable
+    protected $fillable = [
+        'name', 'oib', 'email', 'phone', 'address', 'house_number', 'city', 'postal', 'country_id'
+    ];
+
     public function orders()
     {
         return $this->hasMany('App\Order');
     }
+
 }
