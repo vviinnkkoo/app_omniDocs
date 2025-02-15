dd8af6d197723790daa55a93ebead3a490098a1c
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-02-15 16:45:32 +0100

    Missing usage.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -16,6 +16,7 @@ use App\Models\ProductType;
 use App\Models\Color;
 use App\Models\OrderNote;
 use App\Models\Receipt;
+use App\Models\WorkYears;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\JsonResponse;
