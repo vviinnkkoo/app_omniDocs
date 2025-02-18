1bf2935f3ec26d412998b4f49eadc5ff4ed95966
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-02-18 09:31:33 +0100

    Request only

--- app/Http/Controllers/KprController.php
+++ app/Http/Controllers/KprController.php
@@ -113,7 +113,7 @@ class KprController extends Controller
                 ->withErrors($validator);
         }
 
-        Kpr::create($request->all());
+        Kpr::create($request->$request->only('payer', 'amount', 'origin', 'date', 'info', 'kpr_payment_type_id'));
 
         return redirect('/knjiga-prometa/' . $date->year);
     }
