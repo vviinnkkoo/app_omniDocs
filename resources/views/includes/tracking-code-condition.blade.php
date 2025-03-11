d97fa0f43683404b96e22877bcc2bc134f02e91b
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-03-11 20:52:08 +0100

    Tracking button text change.

--- resources/views/includes/tracking-code-condition.blade.php
+++ resources/views/includes/tracking-code-condition.blade.php
@@ -2,19 +2,19 @@
 
     {{-- GLS --}}
     @if ( App\Models\DeliveryService::find( $order->delivery_service_id)->delivery_company_id == 2 )
-        <a href="https://online.gls-croatia.com/index.php?page=tt_page.php?tt_value={{ $order->tracking_code }}" target="_blank" class="btn btn-sm btn-gls">{{ $order->tracking_code }} <i class="bi bi-arrow-right-circle-fill"></i></a>
+        <a href="https://online.gls-croatia.com/index.php?page=tt_page.php?tt_value={{ $order->tracking_code }}" target="_blank" class="btn btn-sm btn-gls">Provjeri <i class="bi bi-arrow-right-circle-fill"></i></a>
 
     {{-- HP --}}
     @elseif ( App\Models\DeliveryService::find( $order->delivery_service_id)->delivery_company_id == 3 || App\Models\DeliveryService::find( $order->delivery_service_id)->delivery_company_id == 1  )
-        <a href="https://posiljka.posta.hr/hr/tracking/trackingdata?barcode={{ $order->tracking_code }}" target="_blank" class="btn btn-sm btn-hposta">{{ $order->tracking_code }} <i class="bi bi-arrow-right-circle-fill"></i></a>
+        <a href="https://posiljka.posta.hr/hr/tracking/trackingdata?barcode={{ $order->tracking_code }}" target="_blank" class="btn btn-sm btn-hposta">Provjeri <i class="bi bi-arrow-right-circle-fill"></i></a>
 
     {{-- BOXNOW --}}
     @elseif ( App\Models\DeliveryService::find( $order->delivery_service_id)->delivery_company_id == 6 || App\Models\DeliveryService::find( $order->delivery_service_id)->delivery_company_id == 1  )
-        <a href="https://boxnow.hr/?track={{ $order->tracking_code }}" target="_blank" class="btn btn-sm btn-boxnow">{{ $order->tracking_code }} <i class="bi bi-arrow-right-circle-fill"></i></a>
+        <a href="https://boxnow.hr/?track={{ $order->tracking_code }}" target="_blank" class="btn btn-sm btn-boxnow">Provjeri <i class="bi bi-arrow-right-circle-fill"></i></a>
 
     {{-- Tisak --}}
     @elseif ( App\Models\DeliveryService::find( $order->delivery_service_id)->delivery_company_id == 4 )
-        <a href="https://lokator.tisak.hr/PacketStatusForm.aspx?authToken=tisak&code={{ $order->tracking_code }}" target="_blank" class="btn btn-sm btn-tisak">{{ $order->tracking_code }} <i class="bi bi-arrow-right-circle-fill"></i></a>
+        <a href="https://lokator.tisak.hr/PacketStatusForm.aspx?authToken=tisak&code={{ $order->tracking_code }}" target="_blank" class="btn btn-sm btn-tisak">Provjeri <i class="bi bi-arrow-right-circle-fill"></i></a>
         
     @endif
 
