2275c45301a006dd89f3354300220822c29fdf4b
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-03-12 10:48:58 +0100

    Typo -

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -29,7 +29,7 @@ class CustomerController extends Controller
                       ->orWhere('postal', 'like', "%{$search}%")
                       ->orWhere('city', 'like', "%{$search}%")
                       ->orWhereHas('country', function($query) use ($search) {
-                        $query->where('country_name', 'like', "%{$search}%");
+                        $query->where('name', 'like', "%{$search}%");
                       });
             });
         }
