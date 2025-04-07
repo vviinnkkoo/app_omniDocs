c06dba54a28279f7c4ff20766ba037984b663094
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-04-07 18:36:14 +0200

    Wrong field name.

--- resources/views/productTypes.blade.php
+++ resources/views/productTypes.blade.php
@@ -32,7 +32,7 @@
                                 <tr>
                                     <td class="align-middle text-right">{{ $count++ }}</td>
                                     <td class="align-middle text-right">
-                                      <span class="editable" data-id="{{ $productType->id }}" data-field="type_name" data-model="update-product-type">{{ $productType->name }}</span>
+                                      <span class="editable" data-id="{{ $productType->id }}" data-field="name" data-model="update-product-type">{{ $productType->name }}</span>
                                     </td>
                                     <td>
                                       <button class="btn btn-danger delete-btn-x" data-id="{{ $productType->id }}" data-model="delete-product-type"><i class="bi bi-x-lg"></i>
