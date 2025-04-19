ed37cd02653440ff0795acdaf85d1f9e16ddd54f
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-04-19 03:38:25 +0200

    Back to Dejavu Sans

--- resources/views/includes/pdf/style.blade.php
+++ resources/views/includes/pdf/style.blade.php
@@ -25,8 +25,7 @@
     body {
         /*font-family: 'DejaVu Sans';*/
         /*font-family: 'Open Sans', sans-serif;*/
-        /*font-family: 'DejaVu Sans', 'sans-serif';*/
-        font-family: Courier, sans-serif;
+        font-family: 'DejaVu Sans', 'sans-serif';
     }
     h4 {
         margin: 0;
