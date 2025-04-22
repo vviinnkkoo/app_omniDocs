ed016d93c008467b313694d286e1f10e3e4c19f6
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-04-22 11:21:04 +0200

    Full screen.

--- resources/views/pages/orders/show.blade.php
+++ resources/views/pages/orders/show.blade.php
@@ -8,7 +8,7 @@
 @section('content')
 <div class="container">
   <div class="row justify-content-center">
-    <div class="col-md-12">
+    <div class="col-12">
       <div class="card">
 
         {{-- Order info --}}
