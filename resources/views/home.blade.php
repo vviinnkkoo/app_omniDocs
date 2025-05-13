f7102d6261e1b75a6824328e82a577093470ca6f
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-05-13 09:48:53 +0200

    Workyears new update.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,44 +4,43 @@
 
 @section('content')
 <div class="container">
-
-    <div class="container">
-
-      <div class="row">
-        <h1>Status narudžbi</h1>
-        <div class="col-sm">
-              <div class="card text-white bg-success mb-3">
-                <div class="card-header">Sveukupno naručeno: <span class="badge text-bg-warning rounded-pill">{{ $countActiveOrders }}</span></div>
-                <div class="card-body">
-                  <h5 class="card-title">{{ $totalEarnings }} €</h5>
-                  <p class="card-text">...</p>
-                </div>
+  <div class="container">
+    <div class="row">
+      <h1>Status narudžbi</h1>
+      <div class="col-sm">
+            <div class="card text-white bg-success mb-3">
+              <div class="card-header">Sveukupno naručeno: <span class="badge text-bg-warning rounded-pill">{{ $countActiveOrders }}</span></div>
+              <div class="card-body">
+                <h5 class="card-title">{{ $totalEarnings }} €</h5>
+                <p class="card-text">...</p>
               </div>
-        </div>
-        <div class="col-sm">
-              <div class="card text-white bg-primary mb-3">
-                <div class="card-header">Neisporučeno do danas: <span class="badge text-bg-warning rounded-pill">{{ $countUndeliveredOrders }}</span></div>
-                <div class="card-body">
-                  <h5 class="card-title">{{ $undeliveredEarnings }} €</h5>
-                  <p class="card-text">...</p>
-                </div>
-              </div>
-        </div>
-        <div class="col-sm">          
-              <div class="card text-white bg-dark mb-3">
-                <div class="card-header">Naručeno ovaj mjesec: <span class="badge text-bg-warning rounded-pill">{{ $countThisMonthOrders }}</span></div>
-                <div class="card-body">
-                  <h5 class="card-title">{{ $currentMonthEarnings }} €</h5>
-                  <p class="card-text">...</p>
-                </div>
-              </div>
-        </div>
+            </div>
       </div>
+      <div class="col-sm">
+            <div class="card text-white bg-primary mb-3">
+              <div class="card-header">Neisporučeno do danas: <span class="badge text-bg-warning rounded-pill">{{ $countUndeliveredOrders }}</span></div>
+              <div class="card-body">
+                <h5 class="card-title">{{ $undeliveredEarnings }} €</h5>
+                <p class="card-text">...</p>
+              </div>
+            </div>
+      </div>
+      <div class="col-sm">          
+            <div class="card text-white bg-dark mb-3">
+              <div class="card-header">Naručeno ovaj mjesec: <span class="badge text-bg-warning rounded-pill">{{ $countThisMonthOrders }}</span></div>
+              <div class="card-body">
+                <h5 class="card-title">{{ $currentMonthEarnings }} €</h5>
+                <p class="card-text">...</p>
+              </div>
+            </div>
+      </div>
+    </div>
 
-      <div class="row mt-5">
-        <h1>Računi i uplate</h1>
-        
-        @foreach ($workYears as $year)
+    <div class="row mt-3">
+      <h1>Računi i uplate</h1>
+    </div>
+      @foreach ($workYears as $year)
+        <div class="row mt-3">
           <div class="col-sm">
                 <div class="card text-white bg-secondary mb-3">
                   <div class="card-header">Izdano računa u <b>{{ $year }}</b>: <span class="badge text-bg-warning rounded-pill">{{-- App\Http\Controllers\ReceiptController::countReceipts(2023) --}}</span></div>
@@ -60,11 +59,8 @@
                   </div>
                 </div>
           </div>
-        @endforeach
-      </div>
-
-    </div>
+        </div>
+      @endforeach
+  </div>
 </div>
-
-
 @endsection
