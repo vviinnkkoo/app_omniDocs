1658ce2aad5f2f59fb2e159d074904ce79e60fcf
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-06-03 11:07:33 +0200

    Missing usage.

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,12 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Services\GlobalService;
 
 class Customer extends Model
 {
-    use HasFactory;
 
     protected $fillable = [
         'name', 'oib', 'email', 'phone', 'address', 'house_number', 'city', 'postal', 'country_id'
