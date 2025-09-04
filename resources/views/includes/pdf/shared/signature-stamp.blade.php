1ec1e11fed34eadb85f426a62d9133437454dd60
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-09-05 01:04:39 +0200

    Debug img on pdf.

--- resources/views/includes/pdf/shared/signature-stamp.blade.php
+++ resources/views/includes/pdf/shared/signature-stamp.blade.php
@@ -6,12 +6,12 @@
             </td>
             <td class="w-tri center relative-section">
                 M.P.
-                <img src="{{ public_path($appSettings['invoice_stamp']) }}" alt="Omnius Art" height="50" class="mp-image" />
+                {{-- <img src="{{ public_path($appSettings['invoice_stamp']) }}" alt="Omnius Art" height="50" class="mp-image" /> --}}
                 
             </td>
             <td class="w-tri center relative-section">
                 <div style="border-top: solid 1px black;">Potpis</div>
-                <img src="{{ public_path($appSettings['invoice_signature']) }}" alt="Omnius Art" height="50" class="signature-image" />
+                {{-- <img src="{{ public_path($appSettings['invoice_signature']) }}" alt="Omnius Art" height="50" class="signature-image" /> --}}
             </td>
         </tr>
     </table>
