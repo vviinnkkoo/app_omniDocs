8d8edd9ccb2031eb57acce02852b478d91536759
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-09-24 11:03:28 +0200

    Light button color scheme.

--- resources/views/pages/orders/show.blade.php
+++ resources/views/pages/orders/show.blade.php
@@ -464,7 +464,7 @@
               <span style="display: block;">Boja proizvoda:</span>
                 @foreach ($colors as $color)
                   <input type="radio" class="btn-check" name="color_id" autocomplete="off" value="{{ $color->id }} " id="color_{{ $color->id }}" required>
-                  <label class="btn btn-secondary btn-sm me-1 mb-1" for="color_{{ $color->id }}">{{ $color->name }}</label>
+                  <label class="btn btn-light btn-sm me-1 mb-1" for="color_{{ $color->id }}">{{ $color->name }}</label>
                 @endforeach
             </div>
 
