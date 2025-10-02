b02e1ed216ea260c384a24f5983452ef7daff8f2
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-10-02 09:38:57 +0200

    Mass assign

--- app/Models/ProductType.php
+++ app/Models/ProductType.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductType extends Model
 {
+    protected $fillable = [
+        'name'
+    ];
     /*
     |--------------------------------------------------------------------------------------------
     | Relationships
