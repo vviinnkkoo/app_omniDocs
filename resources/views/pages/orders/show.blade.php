daa68e13a027bd632723bd1e2c6be9887d1c3db7
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-10-08 10:19:24 +0200

    Section styling.

--- resources/views/pages/orders/show.blade.php
+++ resources/views/pages/orders/show.blade.php
@@ -1,25 +1,23 @@
 @extends('layouts.app')
 
-@section('title', $order->id . ' - ' . $order->customer_name . ' - Uredi narudžbu' )
+@section('title', $order->id . ' - ' . $order->customer_name . ' - Uredi narudžbu')
 
 @section('content')
-<div class="container-fluid px-2 px-lg-5">
-  <div class="row g-3 justify-content-center">
-    <div class="col-md-12">
-      <div class="card">
-        @include('pages.orders.includes.show.sections.order-info')
+  <div class="container-fluid px-2 px-lg-5">
+    <div class="row g-3 justify-content-center">
+      <div class="col-md-12">
+        <div class="card">
+          @include('pages.orders.includes.show.sections.order-info')
+        </div>
       </div>
+
+      @include('pages.orders.includes.show.sections.order-item-list')
+      @include('pages.orders.includes.show.sections.order-notes')
     </div>
-
-    @include('pages.orders.includes.show.sections.order-item-list')
-    @include('pages.orders.includes.show.sections.order-notes')
-
   </div>
-</div>
 
-@include('pages.orders.includes.show.modals.add-note')
-@include('pages.orders.includes.show.modals.add-product')
-@include('pages.orders.includes.show.modals.add-invoice')
-@include('includes.deleteconfirmation')
-
+  @include('pages.orders.includes.show.modals.add-note')
+  @include('pages.orders.includes.show.modals.add-product')
+  @include('pages.orders.includes.show.modals.add-invoice')
+  @include('includes.deleteconfirmation')
 @endsection
