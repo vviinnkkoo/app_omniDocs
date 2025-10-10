6a27bd009c102e146961a3831e76f72323e9e428
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-10-10 10:24:06 +0200

    Wrong modal target name.

--- resources/views/pages/orders/includes/show/sections/order-packages.blade.php
+++ resources/views/pages/orders/includes/show/sections/order-packages.blade.php
@@ -3,7 +3,7 @@
     <div class="card flex-fill">
         <div class="card-header d-flex align-items-center fw-bolder" style="background-color: #007bff11;">
         <span class="me-2">Paketi</span>
-        <button id="popupButton" class="btn btn-primary btn-sm fw-bolder" data-bs-toggle="modal" data-bs-target="shipmentModal">+</button>
+        <button id="popupButton" class="btn btn-primary btn-sm fw-bolder" data-bs-toggle="modal" data-bs-target="#shipmentModal">+</button>
         </div>
         <div class="card-body" style="border: solid 4px #007bff11;">
             <p class="text-muted">Ovdje idu paketi...</p>
