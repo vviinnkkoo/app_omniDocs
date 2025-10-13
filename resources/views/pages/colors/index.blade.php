9078bf7ce74e3d8c8ca9805f7229c25689d5a09d
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-10-13 11:12:15 +0200

    Styling.

--- resources/views/pages/colors/index.blade.php
+++ resources/views/pages/colors/index.blade.php
@@ -18,7 +18,7 @@
                 <tr>
                   <th scope="col">#</th>
                   <th scope="col">Boja / Opis proizvoda</th>
-                  <th class="col"></th>
+                  <th></th>
                 </tr>
               </thead>
               <tbody>
@@ -28,7 +28,7 @@
                     <td class="align-middle">
                       <span class="editable" data-id="{{ $color->id }}" data-field="name" data-model="opis">{{ $color->name }}</span>
                     </td>
-                    <td class="align-middle w-auto px-2">
+                    <td class="align-middle text-end">
                       <x-delete-button :id="$color->id" model="opis" />
                     </td>
                   </tr>
