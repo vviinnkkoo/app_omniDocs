d0c127b6393e6c2fff4d72c7aa234d4018d579bd
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-10-14 10:32:47 +0200

    HTML modifications.

--- resources/views/pages/orders/index.blade.php
+++ resources/views/pages/orders/index.blade.php
@@ -4,48 +4,39 @@
 
 @section('content')
 <div class="container-fluid px-2 px-lg-5">
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-body">
 
-    <div class="row justify-content-center">
-      <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
+            {{-- New order and customer buttons, search bar section --}}
+          <div class="clearfix mb-3">
+            <button id="popupButton" class="btn btn-primary float-start mb-2" data-bs-toggle="modal" data-bs-target="#newOrderModal">
+              <i class="bi bi-file-earmark-plus"></i> Nova narudžba
+            </button>
+            <button id="popupButton" class="btn btn-primary float-start ms-2 mb-2" data-bs-toggle="modal" data-bs-target="#customerModal">
+              <i class="bi bi-file-earmark-plus"></i> Novi kupac
+            </button>
 
-                   {{-- New order and customer buttons, search bar section --}}
-                  <div class="clearfix mb-3">
-                    <button id="popupButton" class="btn btn-primary float-start mb-2" data-bs-toggle="modal" data-bs-target="#newOrderModal">
-                      <i class="bi bi-file-earmark-plus"></i> Nova narudžba
-                    </button>
-                    <button id="popupButton" class="btn btn-primary float-start ms-2 mb-2" data-bs-toggle="modal" data-bs-target="#customerModal">
-                      <i class="bi bi-file-earmark-plus"></i> Novi kupac
-                    </button>
+            <x-search-form/>
+          </div>
 
-                    <form method="GET" action="{{ $currentUrl }}" class="float-end mb-2">
-                      <div class="input-group">
-                        <input type="text" name="search" class="form-control" placeholder="Upiši traženi pojam..." value="{{ request('search') }}">
-                        <button type="submit" class="btn btn-primary">Pretraži</button>
-                      </div>
-                    </form>
-                  </div>
+          {{-- Order type filter buttons --}}
+          <div class="clearfix mb-3">
+            <a class="btn btn-primary btn-sm float-start" href="/narudzbe/prikaz/sve">Sve narudžbe</a>
+            <a class="btn btn-success btn-sm ms-1 float-start" href="/narudzbe/prikaz/poslane">Poslane narudžbe</a>
+            <a class="btn btn-warning btn-sm ms-1 float-start" href="/narudzbe/prikaz/neodradene">Neodrađene narudžbe</a>
+            <a class="btn btn-danger btn-sm ms-1 float-start" href="/narudzbe/prikaz/otkazane">Otkazane</a>
+          </div>
 
-                  {{-- Order type filter buttons --}}
-                  <div class="clearfix mb-3">
-                    <a class="btn btn-primary btn-sm float-start" href="/narudzbe/prikaz/sve">Sve narudžbe</a>
-                    <a class="btn btn-success btn-sm ms-1 float-start" href="/narudzbe/prikaz/poslane">Poslane narudžbe</a>
-                    <a class="btn btn-warning btn-sm ms-1 float-start" href="/narudzbe/prikaz/neodradene">Neodrađene narudžbe</a>
-                    <a class="btn btn-danger btn-sm ms-1 float-start" href="/narudzbe/prikaz/otkazane">Otkazane</a>
-                  </div>
+          @include('pages.orders.includes.index.sections.order-list')
 
-                  @include('pages.orders.includes.index.sections.order-list')
+          <x-table-pagination :items="$orders" />
 
-                  {{-- Pagination Links --}}
-                  <div class="d-flex justify-content-center">
-                    {{ $orders->appends(['search' => request('search')])->links('pagination::bootstrap-5') }}
-                  </div>
-
-                </div>
-            </div>
         </div>
+      </div>
     </div>
+  </div>
 </div>
 
 @include('pages.orders.includes.index.modals.add-order')
