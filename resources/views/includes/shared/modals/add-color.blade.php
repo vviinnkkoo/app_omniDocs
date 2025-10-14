e94a15a2d2d0a87b17d4144f1fc1985ce6ef8749
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-10-14 09:40:20 +0200

    Color modal extraction.

---
+++ resources/views/includes/shared/modals/add-color.blade.php
@@ -0,0 +1,29 @@
+<!-- New color modal -->
+<div class="modal fade" id="colorModal" tabindex="-1" aria-labelledby="colorModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="colorModalLabel">Nova boja/opis proizvoda</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        {{-- Popup content --}}
+        <form method="POST" action="/opis" id="colorSubmission">
+          {{ csrf_field() }}
+              <div class="form-group">
+                
+                <div class="mb-3">
+                  <label for="color">Boja:</label>
+                  <input type="text" class="form-control" placeholder="Unesi novu boju..." id="color" name="name">
+                </div>
+
+              </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zatvori</button>
+        <button type="submit" class="btn btn-primary" form="colorSubmission">Spremi</button>
+      </div>
+    </div>
+  </div>
+</div>
--- resources/views/pages/colors/index.blade.php
+++ resources/views/pages/colors/index.blade.php
@@ -44,34 +44,7 @@
     </div>
 </div>
 
-<!-- Modal -->
-<div class="modal fade" id="colorModal" tabindex="-1" aria-labelledby="colorModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="colorModalLabel">Nova boja/opis proizvoda</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <!-- popup content -->
-        <form method="POST" action="/opis" id="colorSubmission">
-          {{ csrf_field() }}
-              <div class="form-group">
-
-                  <label for="color">Boja:</label>
-                  <input type="text" class="form-control" placeholder="Unesi novu boju..." id="color" name="name">
-
-              </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zatvori</button>
-        <button type="submit" class="btn btn-primary" form="colorSubmission">Spremi</button>
-      </div>
-    </div>
-  </div>
-</div>
-
+@include('includes.shared.modals.add-color')
 @include('includes.shared.modals.delete-confirmation')
 
 @endsection
