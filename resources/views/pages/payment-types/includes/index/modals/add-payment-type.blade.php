1077929c959042562ded3b36925666b5fb24b760
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-11-04 09:21:40 +0100

    Modal IDs

--- resources/views/pages/payment-types/includes/index/modals/add-payment-type.blade.php
+++ resources/views/pages/payment-types/includes/index/modals/add-payment-type.blade.php
@@ -1,14 +1,14 @@
 <!-- Payment type modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="paymentTypeModalLabel" aria-hidden="true">
+<div class="modal fade" id="paymentTypeModal" tabindex="-1" aria-labelledby="paymentTypeModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="paymentTypeModal">Novi način plaćanja</h5>
+        <h5 class="modal-title" id="paymentTypeModalLabel">Novi način plaćanja</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         {{-- Popup content --}}
-        <form method="POST" action="{{ route('nacin-placanja.store') }}" id="paymentSubmission">
+        <form method="POST" action="{{ route('nacin-placanja.store') }}" id="paymentTypeSubmission">
           @csrf
           <div class="form-group">
 
@@ -23,7 +23,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zatvori</button>
-        <button type="submit" class="btn btn-primary" form="paymentSubmission">Spremi</button>
+        <button type="submit" class="btn btn-primary" form="paymentTypeSubmission">Spremi</button>
       </div>
     </div>
   </div>
