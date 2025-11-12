33d31e29229ac664980c9aed36e6d78a89fede2d
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-11-12 10:58:44 +0100

    Modal button fix.

--- resources/views/pages/orders/includes/show/sections/order-item-list.blade.php
+++ resources/views/pages/orders/includes/show/sections/order-item-list.blade.php
@@ -3,7 +3,7 @@
     <div class="card" style="margin-top: 30px;">
         <div class="card-header d-flex align-items-center fw-bolder" style="background-color: #19875411;">
             <span class="me-2">Proizvodi</span>
-            <x-modal-button target="#addProductModal" text="+"/>
+            <x-modal-button class="btn bg-primary btn-sm" target="#addProductModal" icon="" text="+"/>
         </div>
         <div class="card-body" style=" border: solid 4px #19875411">
             <div class="table-responsive-md">
