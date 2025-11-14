3667c8e97a31172bbd368294862b164a871a0b6f
Davor Vinkešević <52233564+vviinnkkoo@users.noreply.github.com>
2025-11-14 13:51:23 +0100

    Missing render.

--- app/View/Components/Inputs/AdvancedSelect.php
+++ app/View/Components/Inputs/AdvancedSelect.php
@@ -3,35 +3,35 @@
 namespace App\View\Components\Inputs;
 
 use Illuminate\View\Component;
+use Illuminate\View\View;
 
 class AdvancedSelect extends Component
 {
     public $name;
-    public $label;
     public $items;
     public $grouped;
-    public $children;   // NOVO: property ili closure za children
     public $placeholder;
     public $required;
-    public $itemLabel;
+    public $renderItem;
 
     public function __construct(
-        string $name,
-        string $label = '',
-        $items = [],
-        bool $grouped = false,
-        $children = null,         // npr. 'products' ili fn($group) => $group->whatever
-        string $placeholder = 'Pretraži...',
-        bool $required = true,
-        $itemLabel = null
+        $name,
+        $items,
+        $grouped = false,
+        $placeholder = '',
+        $required = false,
+        $renderItem = null
     ) {
         $this->name = $name;
-        $this->label = $label;
         $this->items = $items;
         $this->grouped = $grouped;
-        $this->children = $children;
         $this->placeholder = $placeholder;
         $this->required = $required;
-        $this->itemLabel = $itemLabel;
+        $this->renderItem = $renderItem;
+    }
+
+    public function render(): View
+    {
+        return view('components.inputs.advanced-select');
     }
 }
